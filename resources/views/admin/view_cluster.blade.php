7ef128d0598c8bc3337886be21e2b6aaf9aa4aed
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-02-11 15:46:53 +0500

    sd

--- resources/views/admin/view_cluster.blade.php
+++ resources/views/admin/view_cluster.blade.php
@@ -280,9 +280,9 @@
                                     <div class="form-group">
                                         <label for="exampleFormControlInput1">Select Cluster Image</label>
                                         <input type="file" name="image[]"
-                                            class="form-control @error('image') is-invalid @enderror" value="{{ asset('images') }}/{{ $clus_img1[0] }}" id="Cluster-Name"
+                                            class="form-control @error('image') is-invalid @enderror" value="" id="Cluster-Name"
                                             placeholder="Enter Cluster Image">
-                                        <img src="{{ asset('images') }}/{{ $clus_img1[0] }}" width="100" height="100">
+                                        <img src="{{ asset('images') }}/{{ $clus_img1[0] }}" name="imgg" width="100" height="100">
                                         @error('image')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
