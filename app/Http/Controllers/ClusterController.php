a7676504d69e8024953bdc8fb1c16525635d17fa
naeemliaqatweb <naeemliaqatweb@gmail.com>
2022-02-21 17:57:41 +0500

    ds

--- app/Http/Controllers/ClusterController.php
+++ app/Http/Controllers/ClusterController.php
@@ -638,10 +638,11 @@ class ClusterController extends Controller
     public function deleteCluster($id)
     {
         $clustor = Cluster::find($id);
-        if (\File::exists(public_path('images/' . $clustor->image . ''))) {
-            //dd('File does not exists.');
-            \File::delete(public_path('images/' . $clustor->image . ''));
-        }
+
+        // if (\File::exists(public_path('images/' . $clustor->image . ''))) {
+        //     //dd('File does not exists.');
+        //     \File::delete(public_path('images/' . $clustor->image . ''));
+        // }
         $clustor->delete();
         return redirect()->back()->with('error', 'Cluster Deleted Successfully!');
     }
